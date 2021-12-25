3d887f16c5ca2f4edb851a4df9837f08c699e044
longphamit <longphamjustdoit@gmail.com>
2021-12-25 14:44:41 +0700

    update

--- src/constant/Host.js
+++ src/constant/Host.js
@@ -1,2 +1,2 @@
-// export const HOST="http://103.125.170.20:10045/rest-service/api"
-export const HOST="http://localhost:8765/rest-service/api"
+export const HOST="http://103.125.170.20:10045/rest-service/api"
+//export const HOST="http://localhost:8765/rest-service/api"
